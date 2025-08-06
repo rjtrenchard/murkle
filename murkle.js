b52bb316c13a5d9d1654bee967c9b660c9050cc6
RJ Trenchard <rjtrenchard@gmail.com>
2025-08-06 10:43:34 -0700

    initial commit, add manifest, try document listener

---
+++ murkle.js
@@ -0,0 +1,17 @@
+/**
+ * Murkle
+ * Reddit-new
+ * 
+ * Hides the comment box. Mainly to keep from adding opinions unless you *really* need to add it. 
+ */
+
+const murkle = () => {
+    const dbg = true;
+    const Selector_Path = 'comment-body-header.block > shreddit-async-loader:nth-child(1)';
+
+    const target = document.querySelector(Selector_Path);
+    if (target) target.style.display = 'none';
+    else if (dbg) console.error('No comment body header found.');
+}
+
+document.addEventListener('DataPageReady', murkle);
